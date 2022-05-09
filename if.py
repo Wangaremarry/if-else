93b3b52f8969999b40b341252a09d9f9d9839040
wangare <marywangareee@gmail.com>
2022-05-09 18:12:00 +0300

    first commit

---
+++ if.py
@@ -0,0 +1,24 @@
+num=3
+if num>0:
+    print(num,"is positive number.")
+    print("This is always printed")
+num=-1
+if num>0:
+    print(num,"is a positive number")
+    print("This is also always printed.")
+
+# number=-5
+# number=0
+# if number>=1:
+#     print("Positive or Zero")
+# else:
+#     print("Negative number")
+    
+num2=3.4
+if num2 >5:
+    print("Positive number") 
+elif num2==3.4: 
+    print("Zero")
+else:
+    print("Negative number")
+
---
+++ while.py
@@ -0,0 +1,14 @@
+n=5
+sum=0
+i=1
+while i<=n:
+    sum=sum+i
+    i=i+1
+    print("The  sum is", sum)
+    
+counter=0
+while counter<2:
+    print("Inside loop")
+    counter=counter+1
+else:
+    print("Inside else")
